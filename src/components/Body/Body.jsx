78d2c6dffd232dbaf9bd63b9737e094231198718
Steviggio <steve.mothmora@gmail.com>
2023-09-07 20:32:51 +0200

    Adjusting the body's component display

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,27 +1,29 @@
 import frontendDiag from '../../assets/Frontend/Frontend.png'
-import backendDiag from '../../assets/Backend/Backend.drawio.png'
+import backendDiag from '../../assets/Backend/Backend.png'
 
 const Body = () => {
   return (
     <>
       <main>
-        <div className="flex ">
-          <div className="flex flex-col w-1/2 justify-center gap-5 text-left">
-            <h3 className="pl-3 text-7xl underline">Steviggio</h3>
-            <p className="pl-3  text-2xl">Hey, i'm Steve and i am a React.js / Node.js web developper</p>
-          </div>
-          <div className=" flex flex-col w-1/2 items-center justify-center">
-            <h3 className='text-2xl underline mb-3'>My skills</h3>
-            <div className='h-72 w-9/12 flex flex-col justify-center border rounded-t-xl'>
-              <h4 className='font-semi-bold underline text-lg'>Frontend</h4>
-              <img className='h-60 object-contain' src={frontendDiag} alt="" />
+        <section>
+          <div className="flex ">
+            <div className="flex flex-col w-1/2 justify-center gap-5 text-left">
+              <h3 className="pl-3 text-7xl underline">Steviggio</h3>
+              <p className="pl-3  text-2xl">Hey, i'm Steve and i am a React.js / Node.js web developper</p>
             </div>
-            <div className='h-72 w-9/12 flex flex-col justify-center border rounded-b-xl'>
-              <h4 className='font-semi-bold underline text-lg'>Backend</h4>
-              <img className='h-60 object-contain pt-2' src={backendDiag} alt="" />
+            <div className=" flex flex-col w-1/2 items-center justify-center">
+              <h3 className='text-2xl underline mb-3'>Skills tree</h3>
+              <div className='h-72 w-9/12 flex flex-col justify-center border rounded-t-full'>
+                <h4 className='font-semi-bold underline text-xl'>Frontend</h4>
+                <img className='h-60 object-contain' src={frontendDiag} alt="Frontend diagram" />
+              </div>
+              <div className='h-72 w-9/12 flex flex-col justify-center items-center border rounded-b-full'>
+                <h4 className='font-semi-bold underline text-xl pt-3'>Backend</h4>
+                <img className='h-60 object-contain w-9/12 pb-9' src={backendDiag} alt="Backend diagram" />
+              </div>
             </div>
           </div>
-        </div>
+        </section>
       </main>
     </>
   )
