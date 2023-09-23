3ab4af307f4652e6366249e0ded0a558f9b93dca
Steviggio <steve.mothmora@gmail.com>
2023-09-23 11:44:52 +0200

    Modifying the homepage adress

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
 import React from "react";
 import './App.css'
-import { Link } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+
 
 
 const App = () => {
   return (
-    <>
-      <div>
-        <section className="flex flex-col">
-          <p className="font-bold bg-gray-300"> Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Molestiae asperiores reprehenderit excepturi fugiat libero at
-            voluptatem sapiente autem fuga reiciendis?</p>
-          <Link to="/header">Header</Link>
-          <Link to="/body">Body</Link>
-          <Link to="/footer">Footer</Link>
-          <Link to="/layout">Layout</Link>
-          <Link to="/tiles">Tiles</Link>
-          <Link to="/projects">Projects</Link>
-          <Link to="/skills">skills</Link>
-        </section>
-      </div>
-    </>
+    <Layout />
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Body from './pages/Body/Body.jsx'
-import Header from './components/Layout/Header/Header.jsx'
-import Footer from './components/Layout/Footer/Footer.jsx'
+// import Body from './pages/Body/Body.jsx'
+// import Header from './components/Layout/Header/Header.jsx'
+// import Footer from './components/Layout/Footer/Footer.jsx'
 import './index.css'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import Layout from './components/Layout/Layout.jsx'
-import About from './pages/About/About.jsx'
-import Tiles from './components/Tiles/Tiles.jsx'
-import Projects from './pages/Projects/Projects.jsx'
-import Hover from './components/Hover/Hover.jsx'
-import SkillsList from './components/SkillsList/SkillsList.jsx'
+// import Layout from './components/Layout/Layout.jsx'
+// import Projects from './pages/Projects/Projects.jsx'
+// import Hover from './components/Hover/Hover.jsx'
+// import SkillsList from './components/SkillsList/SkillsList.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<App />} />
-      <Route path="/layout" element={<Layout />} />
-      <Route path="/header" element={<Header />} />
-      <Route path="/footer" element={<Footer />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/tiles" element={<Tiles />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/hover" element={<Hover />} />
-      <Route path="/skills" element={<SkillsList name="Gitlab" />} />
+      <Route path="/portfolio" element={<App />} />
     </>
   ))
 
