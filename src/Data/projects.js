53a0f7df113674f0df025a3a7a0bc0c601e88177
Steviggio <steve.mothmora@gmail.com>
2023-10-04 00:21:54 +0200

    Adding project informations to datas

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -25,35 +25,40 @@ const projectInfos = [{
   usedLanguages: ["HTML", "CSS", "JavaScript"],
   role: "Backend developper",
   usedFrameworks: ["Express.js", "React.js"],
-  tasks: ["Création d'une API REST", "Configuration d'un système d'authentification", "Stockage et sécurisation de données", "Implémentation sécurisée de requêtes CRUD"]
+  description: "Développer une API Restful pour une librairie souhaitant ouvrir un site de référencement et de notation de livre.",
+  tasks: ["Création d'une API REST.", "Configuration d'un système d'authentification.", "Stockage et sécurisation de données.", "Implémentation sécurisée de requêtes CRUD."]
 },
 {
   name: "Booki",
   usedLanguages: ["HTML", "CSS"],
   role: "Frontend developper",
   usedFrameworks: [],
-  tasks: ["Inplémentation d'une interface responsive", "Intégration d'une maquette en HTML/CSS"]
+  description: "Développer l'interface web d'une entreprise souhaitant promouvoir les activités et les hébergement disponibles dans la ville de leur choix.",
+  tasks: ["Inplémentation d'une interface responsive.", "Intégration d'une maquette en HTML/CSS."]
 },
 {
   name: "Kasa",
   usedLanguages: ["HTML", "CSS", "JavaScript"],
   role: "Frontend developper",
   usedFrameworks: ["React"],
-  tasks: ["Création d'une application web avec React.js", "Routing de l'application web avec react-router", "Développement d'une interface web avec Sass"]
+  description: "Développer une application complète en React en suivant les maquettes Figma fournies.",
+  tasks: ["Création d'une application web avec React.js.", "Routing de l'application web avec react-router.", "Développement d'une interface web avec Sass."]
 },
 {
   name: "Sophie Bluel",
   usedLanguages: ["HTML", "CSS", "JavaScript"],
   role: "Frontend developper",
   usedFrameworks: [],
-  tasks: ["Création d'une application web dynamique avec JavaScript", ""]
+  description: "Développer le portfolio d'une architecte d'intérieure.",
+  tasks: ["Création d'une application web dynamique avec JavaScript.", "Intégration des maquettes Figma.", "Implémentation des fonctionnalités d'authentificaiton.", "Implémentation des fonctionnalités d'ajouts de nouveaux élééments à la base de données."]
 },
 {
   name: "Nina Carducci",
   usedLanguages: ["HTML", "CSS", "JavaScript"],
   role: "Frontend developper",
   usedFrameworks: [],
-  tasks: ["Débogage d'une application web", "Optimisation des performances", "Optimisation du SEO"]
+  description: "",
+  tasks: ["Débogage d'une application web.", "Optimisation des performances.", "Optimisation du SEO."]
 }
 ]
 
--- src/components/Modal/ModalElement.jsx
+++ src/components/Modal/ModalElement.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Modal from 'react-modal';
+import BookiHome from "../../assets/projects/Booki.png"
 
 
 const customStyles = {
@@ -49,10 +50,11 @@ const ModalElement = () => {
             <button onClick={closeModal} className='font-bold border-2 p-2 rounded-xl hover:bg-slate-300'>close</button>
             <h2 className=''>Bonsoir</h2></div>
           <div className='flex flex-col w-full'>
-            <p className='text-center'>Voici la div</p>
+            <p className='text-center mb-3 text-2xl'>Projet : Booki </p>
             <div className='flex flex-row gap-10'>
               {/* Image du projet à gauche et description à droite */}
-              <div className='border-4 w-full h-96'>Il va y avoir des photos ici
+              <div className='border-4 w-full h-96'>
+                <img src={BookiHome} alt="" />
               </div>
               <div className='border-4 w-full h-96'>
                 <h3 className='font-bold text-lg pl-3 pt-3'>Description:</h3>
