76dcadfb522f342063379e395c06daba5d28b501
Steviggio <steve.mothmora@gmail.com>
2024-03-18 23:20:19 +0100

    Transition animation for Header elements / Home section modified

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <header >
-      <nav className="flex-col md:flex-row lg:fixed w-full z-10 top-0 drop-shadow-sm border-b border-gray-300 bg-white md:h-20 flex justify-start items-center h-fit py-2 md:py-0">
-        <Link to="/" href="/" className="w-2/6 pb-3 flex justify-center items-center" >
+    <header className="h-20">
+      <nav
+        className={`flex-col md:flex-row lg:fixed w-full z-10 bg-white md:h-20 flex justify-evenly py-2 md:py-0  
+        ${scrolled ? " drop-shadow-sm duration-150" : ""
+          }`}
+      >
+        <Link to="/" href="/" className="w-1/3 pb-3 flex cursor-default items-center">
           <div>
-            <h1 className="steviggio-title font-bold text-4xl">Stevigg<span className="relative bottom-2 left-1 text-2xl">io</span></h1>
+            <h1 className={`transition transform duration-[250ms] steviggio-title cursor-pointer font-bold text-4xl 
+            ${scrolled ? "-translate-y-20 duration-[250ms]" : ""
+              }`}
+
+            >Stevigg<span className="relative bottom-2 left-[0.20rem] text-2xl">io</span></h1>
+            <a href="" className={`absolute top-7 ${scrolled ? "transition-opacity transform ease-in duration-200" : "transition-opacity transform ease-in duration-200 opacity-0 "}`}>Lien</a>
           </div>
         </Link>
-        <div className=" lg:w-4/6 lg:text-xl flex justify-center items-center gap-10 md:gap-11 lg:gap-20">
-          <a href="#body-section" className="nav-link">Accueil</a>
-          <a href="#about-section" className="nav-link">À propos</a>
-          <a href="#projects-section" className="nav-link">Mes projets</a>
-          <a href="#contact-section" className="nav-link">Me contacter</a>
+        <h1 className={`my-auto transition transform duration-[250ms] ${scrolled ? '' : ' -translate-y-20 duration-[250ms]'} steviggio-title cursor-pointer font-bold text-4xl 
+`}
+        >Stevigg<span className="relative bottom-2 left-[0.20rem] text-2xl">io</span></h1>
+        <div className="lg:w-1/3 lg:text-lg flex justify-end items-center gap-10 md:gap-11 ">
+          <button>français</button>
+          <a href="#contact-section" className="nav-link">Contact</a>
         </div>
       </nav>
     </header>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,28 +6,57 @@ const Home = () => {
   return (
     <>
 
-      <section id="body-section" className='bg-home-blue'>
-        <div className="flex flex-col lg:flex-row justify-center items-center gap-8 h-fit lg:h-screen mx-auto w-3/5 py-16">
-          <div className="flex flex-col w-2/3 justify-center my-auto gap-5 text-left">
-            <h3 className="pl-3 text-4xl md:text-5xl lg:text-6xl underline">Steviggio</h3>
-            <TypeAnimation
-              sequence={[
-                "Moi, c'est Steve et je suis un développeur JavaScript spécialisé en React.js & Node.js.",
-                () => {
-                  console.log("Message displayed.");
-                },
-              ]}
-              wrapper="p"
-              speed={80}
-              cursor={true}
-              repeat={1}
-              className='lg:text-2xl text-xl'
-            />
+      <section id="body-section" className='h-screen flex-col gap-10 flex '>
+        <div className="flex flex-row w-fit">
+          <div className="flex flex-col lg:flex-row justify-center items-center ml-16 mt-20 gap-8 h-fit shadow-md rounded-2xl w-3/5 py-16 bg-home-blue
+          select-none
+          ">
+            <div className="flex pl-8 flex-col w-2/3 justify-center my-auto gap-5 text-left ">
+              <h3 className="text-4xl md:text-4xl underline">Steviggio</h3>
+              <TypeAnimation
+                sequence={[
+                  "Moi, c'est Steve et je suis un développeur JavaScript spécialisé en React.js & Node.js.",
+                  // () => {
+                  //   console.log("Message displayed.");
+                  // },
+                ]}
+                wrapper="p"
+                speed={90}
+                cursor={true}
+                repeat={1}
+                className='text-xl'
+              />
+            </div>
+            <div className=" flex flex-col w-1/2 items-center justify-center">
+              <img className='rounded-full w-2/3' src={profilePic} alt="Steviggio's profile picture" />
+            </div>
           </div>
-          <div className=" flex flex-col w-1/2 items-center justify-center">
-            <img className='rounded-full w-2/3' src={profilePic} alt="Steviggio's profile picture" />
+          <div className="flex flex-grow justify-center items-center ">
+            {/* Div containing the development sticker of the differents technos i use (main technos) */}
+            <div className="border-2 w-10 h-10" >
+                
+            </div>
+
           </div>
         </div>
+
+        <div className="flex justify-end">
+          <div className="flex flex-col justify-evenly text-justify shadow-xl rounded-3xl bg-white w-2/4 px-5">
+            <div className="flex flex-row justify-evenly items-center">
+              <div>
+                <h4 className="my-6 text-2xl flex justify-center
+              pr-1 drop-shadow-xl underline underline-offset-2 ">À propos de moi</h4>
+              </div>
+              {/* <img src={profilePic} className="rounded-full w-4/12 " alt={`${profilePic} picture`} /> */}
+            </div>
+            <p className="leading-relaxed lg:px-4 text-sm md:text-lg mb-5 text-left ">
+              Passionné de langues et de culture web, je suis un développeur JS junior ayant démarré l'apprentissage de Javascript en autodidacte en 2020.
+              J'utilise React.js pour créer des interfaces graphiques dynamique et moderne et Node.js pour développer des API RESTful robustes et évolutives.
+
+            </p>
+          </div>
+        </div>
+
       </section>
 
     </>
